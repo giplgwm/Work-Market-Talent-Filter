eedea03c3ccac2dc50ee38e0c2210154a5334f73
gip <98342753+giplgwm@users.noreply.github.com>
2024-06-25 22:06:03 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,15 +2,37 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
     text: "OFF",
   });
+  
+  // Set default settings if not already set
+  chrome.storage.sync.get(['b_nojobs', 'b_nojobscompany', 'b_abandoncompany', 'b_abandonany'], (result) => {
+    if (result.b_nojobs === undefined || result.b_nojobscompany === undefined || result.b_abandoncompany === undefined || result.b_abandonany === undefined) {
+      chrome.storage.sync.set({
+        b_nojobs: false,
+        b_nojobscompany: false,
+        b_abandoncompany: false,
+        b_abandonany: false,
+      });
+    } else {
+      console.log('settings were found...');
+    }
+  });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "get_setting") {
+    chrome.storage.sync.get([request.key], (result) => {
+      sendResponse({ value: result[request.key] });
+    });
+    return true;  // Will respond asynchronously
+  }
 });
 
 const wm_text = 'https://www.workmarket.com';
 
-
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(wm_text)) {
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
     // Set the action badge to the next state
     await chrome.action.setBadgeText({
@@ -19,22 +41,26 @@ chrome.action.onClicked.addListener(async (tab) => {
     });
 
     if (nextState === "ON") {
-      // Execute JS file to check the WM ratings when the user turns the extension on, then inject CSS to hide the profile's we've flagged with a class.
+      // Execute JS file to check the WM ratings when the user turns the extension on, then inject CSS to hide the profiles we've flagged with a class.
       await chrome.scripting.executeScript({
-      target : {tabId : tab.id},
-      files : [ "getprofile_scores.js" ]});
+        target: { tabId: tab.id },
+        files: ["getprofile_scores.js"]
+      });
       await chrome.scripting.insertCSS({
         files: ["hide-bad-wm.css"],
         target: { tabId: tab.id },
       });
 
     } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
+      // Remove the CSS file when the user turns the extension off, and remove the css classes we added.
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["reset-classes.js"]
+      });
       await chrome.scripting.removeCSS({
         files: ["hide-bad-wm.css"],
         target: { tabId: tab.id },
       });
     }
-	
   }
 });
